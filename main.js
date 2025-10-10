dbd21303c811582dbc00a0aae552952aee8f4f3e
Ahmed Gaballah <ahmedgaballah055@gmail.com>
2025-10-10 03:59:25 +0300

    Addded random color and random palette generation functionality.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let initialPalette = ['EF476F', 'FFD166', '06D6A0', '118AB2', '073B4C'];    // initial color palette
 let palette = [...initialPalette];  // current color palette
 
-let paletteColor = document.querySelectorAll('.palette-color');
+let generateBtn = document.getElementById('generate-btn'); // generate button
+let paletteColor = document.querySelectorAll('.palette-color'); // color divs
 
-window.onload = initializePalette();    // initialize color palette
+window.onload = showPalette();    // initialize color palette
 
-// initialize color palette
-function initializePalette() {
+// show color palette
+function showPalette() {
     paletteColor.forEach((colorDiv, index) => {
         colorDiv.style.backgroundColor = `#${palette[index]}`;
     });
@@ -34,10 +35,11 @@ function copyColorCode(id) {
 
 // show toast message
 function showToast(icon, message) {
+    // create toast container
     let toast = document.createElement('div');
     toast.id = 'toast';
     document.body.appendChild(toast);
-
+    // add toast content
     toast.innerHTML = `
         <div class="toast-icon">
             <i class="fa-solid fa-${icon}"></i>
@@ -46,7 +48,7 @@ function showToast(icon, message) {
             ${message}
         </div>
     `;
-
+    // hide toast after 3 seconds
     setTimeout(() => {
         toast.classList.add("close");
     }, 3000);
@@ -54,4 +56,27 @@ function showToast(icon, message) {
         toast.style.display = "none";
     }, 4000);
 
+}
+
+// generate random color
+function generateRandomColor() {
+    const letters = "0123456789ABCDEF"; // hex letters
+    let color = ''; // color string
+    // generate 6 digit hex color
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];   // append random hex letter to color string
+    }
+    return color;
+}
+
+// generate random color palette
+function generateRandomPalette() {
+    palette = [];  // reset palette
+    // generate 5 random colors
+    for (let i = 0; i < 5; i++) {
+        let color = generateRandomColor();  // generate random color
+        palette.push(color);   // add random color to palette
+    }
+    showPalette();    // update color palette
+    showToast('bolt', 'New color palette generated!'); // show toast message
 }
